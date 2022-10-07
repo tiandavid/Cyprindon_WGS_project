56c605e3c3d672dd3a176be7b22933119c767a9d
tiandavid <30607375+tiandavid@users.noreply.github.com>
2022-10-07 15:01:35 -0700

    Create GATK_hard_filter_indels.sh

---
+++ scripts/genotyping_pipeline/GATK_hard_filter_indels.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p savio
+#SBATCH -t 03-00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=davidtian@berkeley.edu
+
+module load java 
+java -jar /global/home/users/davidtian/Genome_assembly_pipeline_scripts/GenomeAnalysisTK.jar \
+    -T VariantFiltration \
+    -R /global/scratch/davidtian/Cyprinodon_ref/asm.racon.fasta \
+    -V V /global/scratch/davidtian/joint_vcfs/all_desert_pupfish/final_variants/ADP_RawINDELS.vcf \
+    --filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
+    --filterName "GATK_indel_filter" \
+    -o /global/scratch/davidtian/joint_vcfs/all_desert_pupfish/final_variants/ADP_gatk_hf_indels.vcf 
